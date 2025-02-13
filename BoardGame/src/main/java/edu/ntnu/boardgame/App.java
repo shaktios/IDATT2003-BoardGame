72d01b5d705d2e339230164400feb74d4d132e63
Shakti Om Sharma <shaktiforgithub@gmail.com>
2025-02-13 02:14:00 +0100

    lagt til dice og die klassene, testet om det funker i App.java

--- BoardGame/src/main/java/edu/ntnu/boardgame/App.java
+++ BoardGame/src/main/java/edu/ntnu/boardgame/App.java
@@ -1,13 +1,14 @@
-package no.ntnu.boardgame;
+package edu.ntnu.boardgame;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) {
+        Dice dice = new Dice(6, 2); // 2 terninger med 6 sider hver
+        System.out.println("Rolled: " + dice.roll());
     }
+
 }
---
+++ BoardGame/src/main/java/edu/ntnu/boardgame/Die.java
@@ -0,0 +1,24 @@
+package edu.ntnu.boardgame;
+
+import java.util.Random;
+
+
+public class Die {
+    protected int sides; 
+    protected int lastRolledValue; 
+    private final Random random; 
+
+    public Die(int sides){
+        this.sides = sides; 
+        this.random = new Random(); 
+    }
+
+    public int roll(){
+        lastRolledValue = random.nextInt(sides) + 1; 
+        return lastRolledValue; 
+    }
+    public int getValue(){
+        return lastRolledValue; 
+    }
+
+}
