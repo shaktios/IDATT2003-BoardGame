198b0458ee31dfe5263c6ff61ee9fb30738a906c
Shakti Om Sharma <shaktiforgithub@gmail.com>
2025-02-14 01:25:09 +0100

    lagd en klasse for tile

---
+++ BoardGame/src/main/java/edu/ntnu/boardgame/Tile.java
@@ -0,0 +1,24 @@
+package edu.ntnu.boardgame;
+
+/* Representerer ett felt på spillbrettet. I denne mappen har vi sagt at felt skal henge etter hverandre.
+Derfor
+
+har et felt alltid en referanse til «neste felt». Dersom det ikke finnes et «neste felt» er feltet det
+siste feltet i spillet (altså mål).
+Et felt kan ha tilknyttet en «aksjon/handling» (engelsk: Action) som blir utført på den spilleren
+som lander på feltet. */
+
+public class Tile {
+    private final int position; //en tile burde være immutabel, posisjonen flyttes ikke på brettet --> bruker derfor final...
+    
+    public Tile(int position){
+        this.position = position; 
+    }
+
+    public int getPosition(){
+        return position; 
+    }
+
+
+    
+}
