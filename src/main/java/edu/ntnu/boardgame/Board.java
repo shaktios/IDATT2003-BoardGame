b399456450a6c88cf4e1878037677ba04542782e
Piranavan Visvalingam <piranavan.visvalingam@outlook.com>
2025-02-26 17:19:30 +0100

    Made a new folder for javafx and more javadocs and fixing according to checkstyle

--- src/main/java/edu/ntnu/boardgame/Board.java
+++ src/main/java/edu/ntnu/boardgame/Board.java
@@ -4,30 +4,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
+  private final List<Tile> tiles; 
 
-    private final List<Tile> tiles; 
-
-    public Board(int size){
-        if (size <= 0) {
-            throw new IllegalArgumentException("Board-størrelsen må være større enn 0.");
-        }
-
-        this.tiles = new ArrayList<>(); 
-        for (int i = 1; i <= size; i++) {
-            tiles.add(new Tile(i)); 
-        }
+  public Board(int size){
+    if (size <= 0) {
+      throw new IllegalArgumentException("Board-størrelsen må være større enn 0.");
     }
 
-    public Tile getTile(int position){
-        if (position >= 1 && position <= tiles.size()) {
-            return tiles.get(position - 1);
-        }
-        throw new IllegalArgumentException("Ugyldig posisjon: " + position);
+    this.tiles = new ArrayList<>(); 
+    for (int i = 1; i <= size; i++) {
+      tiles.add(new Tile(i)); 
     }
+  }
 
-    public int getSize() {
-        return tiles.size();
+  public Tile getTile(int position){
+    if (position >= 1 && position <= tiles.size()) {
+      return tiles.get(position - 1);
     }
+    throw new IllegalArgumentException("Ugyldig posisjon: " + position);
+  }
+
+  public int getSize() {
+    return tiles.size();
+  }
 
     
 }
