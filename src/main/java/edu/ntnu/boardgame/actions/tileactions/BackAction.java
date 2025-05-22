474be8a56f03c525b442e02d32d7581d31d9964a
Shakti Om Sharma <shaktiforgithub@gmail.com>
2025-05-22 17:54:19 +0200

    added javadocs and tests for tileactions

--- src/main/java/edu/ntnu/boardgame/actions/tileactions/BackAction.java
+++ src/main/java/edu/ntnu/boardgame/actions/tileactions/BackAction.java
@@ -4,19 +4,41 @@ import edu.ntnu.boardgame.constructors.Board;
 import edu.ntnu.boardgame.constructors.Boardgame;
 import edu.ntnu.boardgame.constructors.Player;
 
+/**
+ * A tile action that moves the player backwards to a specified destination
+ * tile. Typically used for "snakes" or negative effects in the game.
+ */
 public class BackAction implements TileAction {
 
   private final int destination;
   private Boardgame boardgame;
 
+    /**
+     * Creates a new BackAction that moves the player to the given destination
+     * tile.
+     *
+     * @param destination the tile number the player should move to
+     */
   public BackAction(int destination) {
         this.destination = destination;
     }
 
+    /**
+     * Sets the boardgame instance so the action can trigger win checking.
+     *
+     * @param boardgame the boardgame context
+     */
   public void setBoardgame(Boardgame boardgame) {
         this.boardgame = boardgame;
     }
 
+    /**
+     * Moves the player to the destination tile and calls checkForWin if
+     * boardgame is set.
+     *
+     * @param player the player affected by the action
+     * @param board the game board
+     */
   @Override
   public void execute(Player player, Board board) {
     player.setPosition(destination, board);
@@ -25,6 +47,13 @@ public class BackAction implements TileAction {
     }
   }
 
+
+
+    /**
+     * Returns the destination tile number the player will be moved to.
+     *
+     * @return the destination tile number
+     */
   @Override
   public int getDestination() {
         return destination;
--- src/main/java/edu/ntnu/boardgame/actions/tileactions/LadderAction.java
+++ src/main/java/edu/ntnu/boardgame/actions/tileactions/LadderAction.java
@@ -4,19 +4,46 @@ import edu.ntnu.boardgame.constructors.Board;
 import edu.ntnu.boardgame.constructors.Boardgame;
 import edu.ntnu.boardgame.constructors.Player;
 
+
+/**
+ * A tile action that moves the player forward to a specific destination tile.
+ * Used to represent "ladders" in ladder-based board games.
+ */
 public class LadderAction implements TileAction {
 
   private final int destination;
-  private Boardgame boardgame; // Ikke final
+  private Boardgame boardgame; 
 
+
+    /**
+     * Constructs a LadderAction that moves the player to a given destination
+     * tile.
+     *
+     * @param destination the tile number to move the player to
+     */
   public LadderAction(int destination) {
         this.destination = destination;
     }
 
+
+    /**
+     * Injects a Boardgame instance used for win condition checking after
+     * executing the action.
+     *
+     * @param boardgame the boardgame context
+     */
   public void setBoardgame(Boardgame boardgame) {
         this.boardgame = boardgame;
     }
 
+
+    /**
+     * Executes the ladder action by moving the player to the destination tile
+     * and optionally checking for game win.
+     *
+     * @param player the player landing on the tile
+     * @param board the game board the player is on
+     */
   @Override
   public void execute(Player player, Board board) {
     player.setPosition(destination, board);
@@ -25,6 +52,12 @@ public class LadderAction implements TileAction {
     }
   }
 
+
+    /**
+     * Returns the destination tile this action leads to.
+     *
+     * @return the destination tile number
+     */
   @Override
   public int getDestination() {
         return destination;
